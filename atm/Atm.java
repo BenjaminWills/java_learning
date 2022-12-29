15e70418337f0b3d9a04a67e6c542270157a9ef9
Benjaminwills <benjaminwills047@gmail.com>
2022-12-29 15:30:16 +0000

    added pin validation

--- atm/Atm.java
+++ atm/Atm.java
@@ -1,8 +1,14 @@
 package atm;
 
 public class Atm {
-    
-    public boolean accessUser(){
-        return true;
+    String storagePath = "storage/users.txt";
+
+    public boolean validatePin(String pin,User atmUser){
+        if (pin == atmUser.pin){
+            System.out.println("Access granted to: " + atmUser.firstName + " " + atmUser.lastName);
+            return true;
+        }else{
+            return false;
+        }
     }
 }
