81aefe4381dc3662c8c276ca10462ade5a37f9f1
Benjaminwills <benjaminwills047@gmail.com>
2022-12-29 14:11:43 +0000

    testing

--- atm/User.java
+++ atm/User.java
@@ -1,7 +1,19 @@
 package atm;
 
 public class User {
-    float balance;
+    float balance = 0;
     String firstName,lastName;
-    int age, pin;
+    int age, id;
+    String pin = "0000";
+
+    public User(String userFirstName, String userLastName, int userAge){
+        this.firstName = userFirstName;
+        this.lastName = userLastName;
+        this.age = userAge;
+    }
+
+    public static void main(String[] args){
+        User me = new User("Ben","Wills",21);
+        System.out.println(me.age);
+    }
 }
