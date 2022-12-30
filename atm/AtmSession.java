5c23762d3e285596b801cf2e84e178cef6b981e3
Benjaminwills <benjaminwills047@gmail.com>
2022-12-30 10:00:20 +0000

    added user as an attribute

--- atm/AtmSession.java
+++ atm/AtmSession.java
@@ -5,23 +5,25 @@ import atm.StorageSystem;
 
 public class AtmSession {
     String id,pin;
+    HashMap<String,Object> user;
     boolean accessGranted = false;
 
     public AtmSession(String id, String pin){
         this.id = id;
         this.pin = pin;
+        this.user = StorageSystem.getUserFromId(this.id,"atm/storage");
 
         try{
         this.accessGranted = validatePin();
         }catch (Exception e){
             e.printStackTrace();
         }
+
     }
 
     public boolean validatePin() throws Exception{
-        HashMap<String,Object> atmUser = StorageSystem.getUserFromId(this.id,"atm/storage");
-        if (this.pin.equals(atmUser.get("pin"))){
-            System.out.println("Access granted to: " + atmUser.get("firstName") + " " + atmUser.get("lastName"));
+        if (this.pin.equals(this.user.get("pin"))){
+            System.out.println("Access granted to: " + this.user.get("firstName") + " " + this.user.get("lastName"));
 
         }else{
             System.out.println("Access denied");
@@ -30,7 +32,10 @@ public class AtmSession {
         return true;
     }
 
+    public void deposit(float depositAmount){}
+    public void withdrawal(float withdrawalAmount){}
+
     public static void main(String[] args){
-        AtmSession session = new AtmSession("caf99fac-8a4c-44d9-b680-c7f3fe084eb1","0100");
+        AtmSession session = new AtmSession("caf99fac-8a4c-44d9-b680-c7f3fe084eb1","0000");
     }
 }
