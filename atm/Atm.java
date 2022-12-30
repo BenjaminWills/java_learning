4f376a4010238e85380041aa0a32d29c2e6dfe38
Benjaminwills <benjaminwills047@gmail.com>
2022-12-30 10:43:01 +0000

    implemented atm

--- atm/Atm.java
+++ atm/Atm.java
@@ -1,5 +1,29 @@
 package atm;
+import java.util.Scanner;
+import atm.AtmSession;
 
 public class Atm {
     String storagePath = "storage/users.txt";
+    static boolean sessionInProgress = true;
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Pin");
+        String userPin = scanner.nextLine();
+        AtmSession session = new AtmSession("90d999c3-8d9c-43a4-a990-7d2460074475",userPin);
+        while (sessionInProgress) {
+            System.out.println("Would you like to deposit today? (true or false)");
+            Boolean userResponse = scanner.nextBoolean();
+            if (userResponse){
+                System.out.println("How much would you like to deposit?");
+                float userAmount = scanner.nextFloat();
+                session.deposit(userAmount);
+                System.out.println(session.user);
+            }else{
+                System.out.println("Goodbye");
+                break;
+            }
+        }
+        scanner.close();
+    }
 }
